6411494f93b1981a24ee20d4e3ac54b1dd85e2c1
Nit0Nik0 <mattpoin96@gmail.com>
2020-11-29 22:42:34 -0500

    scope steps completed basic functionality working

--- Main.js
+++ Main.js
@@ -7,9 +7,9 @@ You are a 1* on a quest to 2* while 3*.
 */
 
 //Test values. TBD Convert to csv ?
-let testValue1 = ["disfunctional group of bards" , "rabid group of goblins", "some guy named Steve" ];
-let testValue2 = ["stop a vauge prophecy", "kill the deamon lord", "rob the capital bank"];
-let testValue3 = ["The town gaurds are on your tail.","a members old fling wants reveng", "the fey are out for blood."]; 
+let groupNounValue = ["disfunctional group of bards" , "rabid group of goblins", "guy named Steve" ];
+let objectiveValue = ["stop a vauge prophecy", "kill the deamon lord", "rob the capital bank"];
+let conflictValue = ["The town gaurds are on your tail","an old fling wants revenge", "the fey are out for blood"]; 
 
 let generator = {
     _groupNoun : "",
@@ -17,13 +17,14 @@ let generator = {
     _conflict : "",
 
    set groupNoun(value){
+       console.log('hit');
         this._groupNoun = value;
    },
    set objective(value){
         this._objective = value;
    },
    set conflict(value){
-       this._objective = value
+       this._conflict = value
    },
    get groupNoun(){
         return this._groupNoun;
@@ -35,10 +36,30 @@ let generator = {
         return  this._conflict;
    },
    generatePlot(){  
-    this._groupNoun = this.generateWord('groupNoun');
+    this._groupNoun = this.getWord('groupNoun');
+    this._objective = this.getWord('objective');
+    this._conflict  = this.getWord('conflict');
+
+    return `You are a ${this._groupNoun} on a quest to ${this._objective} while ${this._conflict}.`
     },
-   generateWord(wordType){ 
-   }
+
+    getWord(wordType){
+        switch(wordType){
+            case 'groupNoun':
+                return groupNounValue[Math.floor( Math.random() * groupNounValue.length )]
+                break;
+            case 'objective':
+                return objectiveValue[Math.floor( Math.random() * objectiveValue.length )]
+                break;
+            case 'conflict':
+                return conflictValue[Math.floor( Math.random() * conflictValue.length )]
+                break;
+            default:
+                console.log('Something broke')
+        }
+    }
 
    
 };
+
+console.log(generator.generatePlot());
